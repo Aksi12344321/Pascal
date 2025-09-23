35d52f0c4f46fc8c06f77532c20f3ef7fe51ebb2
Kseniya Kostromina <aksi1234@yandex.ru>
2025-09-23 19:25:40 +0300

    fix

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -2,11 +2,20 @@ function mobileNav() {
   // Mobile nav button
   const navBtn = document.querySelector(".mobile-nav-btn");
   const nav = document.querySelector(".mobile-nav");
+  const navLinks = document.querySelectorAll(".mobile-nav__list a");
 
   navBtn.addEventListener("click", () => {
     nav.classList.toggle("mobile-nav--open");
     navBtn.classList.toggle("mobile-nav-btn--active");
     document.body.classList.toggle("no-scroll");
+  });
+
+  navLinks.forEach((item) => {
+    item.addEventListener("click", () => {
+      nav.classList.remove("mobile-nav--open");
+      navBtn.classList.remove("mobile-nav-btn--active");
+      document.body.classList.remove("no-scroll");
+    });
   });
 }
 
